bfd976f24c51282dfc9917e37f27eafe3c66d691
hyldrix <117600898+hyldrix@users.noreply.github.com>
2025-03-16 12:11:39 +0100

    Add Upcoming Classes component in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Home from './components/home/Home.jsx';
 import NotFound from './components/404/404.jsx';
 import Login from './components/login/Login.jsx';
 import Register from './components/register/Register.jsx';
+import UpcomingClasses from './components/upcoming-classes/UpcomingClasses.jsx';
 import './App.css';
 import { AuthContextProvider } from './contexts/AuthContext.jsx';
 
@@ -19,6 +20,7 @@ function App() {
             <Route exact path="/" element={<Home />} />
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/register" element={<Register />} />
+            <Route exact path="/classes" element={<UpcomingClasses />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
