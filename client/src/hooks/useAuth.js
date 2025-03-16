635658db1fd58eec098af4855cc4778839014f14
hyldrix <117600898+hyldrix@users.noreply.github.com>
2025-03-16 12:08:22 +0100

    Fix logout

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,4 +1,4 @@
-import { login, register } from '../api/auth-api.js';
+import { login, logout, register } from '../api/auth-api.js';
 import { useAuthContext } from '../contexts/AuthContext.jsx';
 
 export const useLogin = () => {
@@ -6,6 +6,7 @@ export const useLogin = () => {
     const loginHandler = async (email, password) => {
         const authData = await login(email, password);
         changeAuthState(authData);
+
         return authData;
     };
 
@@ -22,10 +23,12 @@ export const useRegister = () => {
     const registerHandler = async (email, password, username) => {
 
 
+
         const authData = await register(email, password, username);
         console.log(authData);
         authData.password = null;
         changeAuthState(authData)
+
         return authData;
 
     };
@@ -36,11 +39,14 @@ export const useRegister = () => {
 
 export const useLogout = () => {
 
+    const { changeAuthState } = useAuthContext();
+
     const { logout: localLogout } = useAuthContext();
 
     const logoutHandler = async () => {
-        await logout();
         localLogout();
+        await logout();
+        
     };
 
     return logoutHandler;
