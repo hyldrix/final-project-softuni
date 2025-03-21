7934503c63cc439556f4ed27c80a354051365cff
hyldrix <117600898+hyldrix@users.noreply.github.com>
2025-03-21 11:06:27 +0100

    Add new components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,25 +7,36 @@ import NotFound from './components/404/404.jsx';
 import Login from './components/login/Login.jsx';
 import Register from './components/register/Register.jsx';
 import UpcomingClasses from './components/upcoming-classes/UpcomingClasses.jsx';
+import CreateClasses from './components/create-classes/CreateClasses.jsx';
 import './App.css';
 import { AuthContextProvider } from './contexts/AuthContext.jsx';
+import ClassDetailsCard from './components/upcoming-classes/ClassDetailsCard/ClassDetailsCard.jsx';
+import EditClasses from './components/edit-classes/EditClasses.jsx';
+import ManageClasses from './components/manage-classes/ManageClasses.jsx';
+import MyClasses from './components/my-classes/MyClasses.jsx';
 
 function App() {
 
   return (
-      <AuthContextProvider>
-        <NavBar />
-        <div id="main">
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/register" element={<Register />} />
-            <Route exact path="/classes" element={<UpcomingClasses />} />
+    <AuthContextProvider>
+      <NavBar />
+      <div id="main">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/classes" element={<UpcomingClasses />} />
+          <Route exact path="/classes/:classId" element={<ClassDetailsCard/>} />
+            <Route exact path="/classes/create" element={<CreateClasses/>} />
+            <Route exact path="/classes/edit/:classId" element={<EditClasses/>} />
+            <Route exact path="/classes/admin/" element={<ManageClasses/>} />
+            <Route exact path="/myclasses" element={<MyClasses/>} />
+  
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </div>
-        <Footer />
-      </AuthContextProvider>
+      </div>
+      <Footer />
+    </AuthContextProvider>
   );
 }
 
