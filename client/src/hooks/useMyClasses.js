c6c23b932c08aa4b397df48897d805b2c761f709
hyldrix <117600898+hyldrix@users.noreply.github.com>
2025-03-21 11:19:41 +0100

    Add custom hooks useGetMyClasses and useGetAllClasses
    Both components are using reducer and provide added functionality for components MyClasses & Admin Panel

---
+++ client/src/hooks/useMyClasses.js
@@ -0,0 +1,74 @@
+import { useEffect, useReducer, useState } from 'react';
+import { getSignedClasses } from '../api/classes-api.js';
+function classesReducer(state, action) {
+    switch (action.type) {
+        case 'GET_ALL_MY_CLASSES':
+            return action.payload;
+        case 'DELETE_CLASS': {
+            let newState = state.filter((classItem) => classItem.classId !== action.payload.id);
+            return [...newState];
+            if (newState) {
+                return [...state];
+            }
+
+            return newState;
+        };
+        
+        default:
+            return state;
+    }
+}
+export function useGetMyClasses(userId) {
+
+    const [isLoading, setIsLoading] = useState(false);
+
+       const [classes, dispatch] = useReducer(classesReducer, []);
+
+    useEffect(() => {
+        (async () => {
+            setIsLoading(true);
+            try {
+                const classes = await getSignedClasses(userId);
+                console.log(classes);
+                dispatch({ type: 'GET_ALL_MY_CLASSES', payload: classes });
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setTimeout(() => setIsLoading(false), 1500);
+
+            }
+        })();
+
+    }, [userId]);
+
+    return [classes, dispatch, isLoading];
+
+}
+export function useGetAllClasses(userId) {
+
+    const [isLoading, setIsLoading] = useState(false);
+
+   
+
+    const [classes, dispatch] = useReducer(classesReducer, []);
+
+    useEffect(() => {
+        (async () => {
+            setIsLoading(true);
+            try {
+                const classes = await getSignedClasses(userId);
+                console.log(classes);
+                dispatch({ type: 'GET_ALL_MY_CLASSES', payload: classes });
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setTimeout(() => setIsLoading(false), 1500);
+
+            }
+        })();
+
+    }, [userId]);
+
+    return [classes, dispatch, isLoading];
+
+}
