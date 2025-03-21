1f08ab272f871d617ca8964f380a6e14a242381c
hyldrix <117600898+hyldrix@users.noreply.github.com>
2025-03-21 11:17:01 +0100

    Fix accessToken -> persisted state

--- client/src/utils/authUtils.js
+++ client/src/utils/authUtils.js
@@ -1,11 +1,27 @@
 export const getAccessToken = () => {
-    const authJSON = localStorage.getItem('accessToken');
+    const authJSON = localStorage.getItem('auth');
 
     if (!authJSON) {
         return '';
+    } else {
+        return JSON.parse(authJSON).accessToken;
     }
 
-    
 
-    return authJSON;
+
+};
+
+export const isAdmin = () => {
+    const authJSON = localStorage.getItem('auth');
+
+    let result = false;
+
+    if (!authJSON) {
+        result = false;
+    } else {
+        result = JSON.parse(authJSON)._id;
+    }
+    console.log(result === '60f0cf0b-34b0-4abd-9769-8c42f830dffc');
+    return result === '60f0cf0b-34b0-4abd-9769-8c42f830dffc';
+
 };
